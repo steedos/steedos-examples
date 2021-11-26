5b58c1ed3f079151521533187b01724a14d8e0ef
廖大雪 <2291335922@qq.com>
2021-11-26 13:32:46 +0800

    优化api/report 接口代码

--- steedos-packages/meeting-examples/main/default/routes/report.router.js
+++ steedos-packages/meeting-examples/main/default/routes/report.router.js
@@ -1,18 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const http = require('http');
+const objectql = require('@steedos/objectql');
 
 router.get('/api/report/:filename', async function (req, res) {
     const query = req.query;
     const bodyStr = Buffer.from(query.q, 'base64').toString('utf-8');
     const body = JSON.parse(bodyStr);
-    if(body.data.dataSource){
-        const data = await dataSource[body.data.dataSource].get(body.data.ids);
-        body.data.data = data
-    }
-    body.data.graphql_url = `${process.env.ROOT_URL}/graphql`;// Meteor.absoluteUrl('/graphql')
-    body.data.root_url = `${process.env.ROOT_URL}`;
-    const url = `${Meteor.settings.public.webservices.jsreport.url}/api/report`;
+    const config = objectql.getSteedosConfig();
+    body.data.graphql_url = Steedos.absoluteUrl('/graphql');
+    body.data.root_url = Steedos.absoluteUrl();
+    const url = `${config.public.webservices.jsreport.url}/api/report`;
     const proxyReq = http.request(url, {
         method:'POST', 
         headers: {

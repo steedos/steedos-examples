08365923d1e7a1906c5e2d048066afbfd486cde8
殷亮辉 <yinlianghui@hotoa.com>
2022-07-21 14:48:33 +0000

    beforeFind示例 https://github.com/steedos/steedos-platform/issues/3592

--- with-object-trigger/steedos-app/main/default/triggers/meeting.trigger.js
+++ with-object-trigger/steedos-app/main/default/triggers/meeting.trigger.js
@@ -1,5 +1,7 @@
 const objectql = require('@steedos/objectql');
 const manager = require('../manager/meeting');
+const auth = require("@steedos/auth");
+const Filters = require('@steedos/filters');
 
 module.exports = {
     listenTo: 'meeting__c',
@@ -30,7 +32,32 @@ module.exports = {
     },
 
     beforeFind: async function(){
-    
+        const query = this.query;
+        const userId = this.userId;
+        const spaceId = this.spaceId;
+        if (userId && spaceId) {
+            //获取原始的查询条件
+            let filters = query.filters;
+            //自定义生成用户权限查询条件
+            let permissionsFilters;
+            const userSession = await auth.getSessionByUserId(userId, spaceId);
+            const roles = userSession.roles;//权限集及简档集合
+            var isAdmin = userSession.is_space_admin;
+            // 除工作区管理员及名为meeting_admin的权限集下用户外，其他用户只能查看自己创建或分配给自己的会议
+            if (!isAdmin && !roles.indexOf("meeting_admin")) {
+                permissionsFilters = [
+                    ["owner", "=", userId],
+                    "or",
+                    ["staff__c", "=", userId],
+                    "or",
+                    ["dining_executive__c", "=", userId]
+                ];
+            }
+            if(permissionsFilters){
+                //修改查询条件; formatFiltersToODataQuery函数将数组形式的filters转换为字符串filters
+                query.filters = `(${filters}) and (${Filters.formatFiltersToODataQuery(permissionsFilters)})`;
+            }
+        }
     },
 
     afterInsert: async function(){

0fd7d034577df88636c84e688ddb54d703995e6e
Byran <sunhaolin@hotoa.com>
2022-07-21 17:37:21 +0800

    Update test_webhooks.router.js

--- with-webhooks/steedos-app/main/default/routes/test_webhooks.router.js
+++ with-webhooks/steedos-app/main/default/routes/test_webhooks.router.js
@@ -2,7 +2,7 @@
  * @Author: sunhaolin@hotoa.com
  * @Date: 2022-07-21 16:19:42
  * @LastEditors: sunhaolin@hotoa.com
- * @LastEditTime: 2022-07-21 17:26:12
+ * @LastEditTime: 2022-07-21 17:37:08
  * @Description: 
  */
 const express = require("express");
@@ -17,6 +17,7 @@ router.post('/api/test/webhooks', async function (req, res) {
     try {
         console.log(chalk.blue('/api/test/webhooks called!!'))
         console.log(req.body)
+        // TODO 如：发送短信给当前处理人、推送消息到第三方等
         res.status(200).send({ success: true });
     } catch (error) {
         res.status(200).send({ success: false, error: error.message });

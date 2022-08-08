1a86eb5b9c3ab27d898fda5505c9e279f1980290
Byran <sunhaolin@hotoa.com>
2022-08-08 09:40:22 +0000

    外部数据源示例 examples/with-postgresql https://github.com/steedos/steedos-platform/issues/3685

---
+++ with-postgresql/steedos-app/main/default/triggers/keycloak_users.trigger.js
@@ -0,0 +1,34 @@
+const objectql = require('@steedos/objectql');
+const { v4: uuidv4 } = require('uuid')
+
+module.exports = {
+    listenTo: 'keycloak_users',
+
+    beforeInsert: async function(){
+        const doc = this.doc
+        if (!doc.id) {
+            doc.id = uuidv4()
+        }
+    },
+
+    beforeUpdate: async function(){
+    
+    },
+
+    beforeDelete: async function(){
+    
+    },
+
+    afterInsert: async function(){
+    
+    },
+
+    afterUpdate: async function(){
+    
+    },
+
+    afterDelete: async function(){
+    
+    },
+
+}
--- with-postgresql/steedos.config.js
+++ with-postgresql/steedos.config.js
@@ -1,16 +1,30 @@
 module.exports = {
 	// Namespace of nodes to segment your nodes on the same network.
 	namespace: "steedos",
-	
+
 	// Default log level for built-in console logger. It can be overwritten in logger options above.
 	// Available values: trace, debug, info, warn, error, fatal
-	logLevel: "info",
+	logLevel: "warn",
 
-    tracing: false,
+	tracing: false,
 
 	// Called after broker started.
 	started(broker) {
 		broker.createService(require("@steedos/service-enterprise"));
 	},
 
+	settings: {
+		datasources: {
+			postgres: {
+				connection: {
+					"driver": "postgres",
+					"host": "127.0.0.1",
+					"username": "keycloak",
+					"password": "keycloak",
+					"database": "keycloak",
+					"port": 5432
+				}
+			}
+		}
+	}
 };
